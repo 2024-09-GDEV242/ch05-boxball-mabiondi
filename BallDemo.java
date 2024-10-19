2580a56423d756719529f3204702bc43ae58ae3e
mabiondi <mbiondi189@gmail.com>
2024-10-19 00:42:35 -0400

    Put balls in demo inside walls

--- BallDemo.java
+++ BallDemo.java
@@ -66,9 +66,9 @@ public class BallDemo
         myCanvas.drawLine(50, ground, 550, ground);
 
         // create and show the balls
-        BoxBall ball = new BoxBall(50, 50, 16, Color.BLUE, ground, top, left, right, myCanvas);
+        BoxBall ball = new BoxBall(150, 150, 16, Color.BLUE, ground, top, left, right, myCanvas);
         ball.draw();
-        BoxBall ball2 = new BoxBall(70, 80, 20, Color.RED, ground, top, left, right, myCanvas);
+        BoxBall ball2 = new BoxBall(170, 180, 20, Color.RED, ground, top, left, right, myCanvas);
         ball2.draw();
 
         while (true) {
