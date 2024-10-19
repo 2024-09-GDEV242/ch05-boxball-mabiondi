539c488e8844f598763a6a779b9e6ed5aa84be8e
mabiondi <mbiondi189@gmail.com>
2024-10-19 00:24:52 -0400

    Started boxBounce() method for box demo

--- BallDemo.java
+++ BallDemo.java
@@ -51,4 +51,27 @@ public class BallDemo
             }
         }
     }
+    
+    public void boxBounce()
+    {
+        int ground = 400;   // position of the ground line
+
+        myCanvas.setVisible(true);
+
+        // draw the ground
+        myCanvas.setForegroundColor(Color.BLACK);
+        myCanvas.drawLine(50, ground, 550, ground);
+
+        // create and show the balls
+        BoxBall ball = new BoxBall(50, 50, 16, Color.BLUE, ground, myCanvas);
+        ball.draw();
+        BoxBall ball2 = new BoxBall(70, 80, 20, Color.RED, ground, myCanvas);
+        ball2.draw();
+
+        while (true) {
+            myCanvas.wait(50);           // small delay
+            ball.move();
+            ball2.move();
+        }
+    }
 }
